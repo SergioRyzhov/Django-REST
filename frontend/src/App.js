89a57275e7d396ffd29f4d3be5e78558a42f9570
Sergey Ryzhov <s.a.ryzhov@gmail.com>
2021-12-14 17:41:03 +0300

    Add request to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import UserList from './components/UserList.js';
+import axios from 'axios'
 
 class App extends React.Component{
   constructor(prop) {
@@ -7,6 +8,18 @@ class App extends React.Component{
     this.state = {
       'users': []
     }
+  }
+
+  componentDidMount() {
+      axios
+          .get('http://127.0.0.1:8000/api/users/')
+          .then(response => {
+              const users = response.data
+              this.setState({
+                  'users': users
+              })
+          })
+          .catch(error => console.log(error))
   }
 
   render (){
